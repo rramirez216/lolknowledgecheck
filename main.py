655da001512be1d6886fb31d95753ffb4d8c97ec
Ruben Ramirez <rramirez110911@gmail.com>
2025-06-04 22:53:33 -0700

    tests the champion object

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def main():
     champion_list = fetch(
         "http://ddragon.leagueoflegends.com/cdn/12.6.1/data/en_US/champion.json"
     )
+    for x in champion_list:
+        print(champion_list[x]["title"])
 
 
 if __name__ == "__main__":
