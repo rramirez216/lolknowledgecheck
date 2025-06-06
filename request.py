f4b7399e429127f494852996624d8f99e9f18ab5
Ruben Ramirez <rramirez110911@gmail.com>
2025-06-05 22:01:29 -0700

    creates a new dictionary from the data object

--- request.py
+++ request.py
@@ -2,5 +2,14 @@ import requests
 
 
 def fetch(url):
-    data = requests.get(url).json()
-    return data["data"]
+    response = requests.get(url).json()
+    data = response["data"].values()
+    new_list = map(
+        lambda key: {"id": key["id"], "name": key["name"], "tags": key["tags"]},
+        data,
+    )
+    # return list(response["data"].values())
+    print(list(new_list))
+
+
+fetch("http://ddragon.leagueoflegends.com/cdn/12.6.1/data/en_US/champion.json")
