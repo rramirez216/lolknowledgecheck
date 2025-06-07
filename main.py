8c951b5f940cde839419788d29283b491ffa48e5
Ruben Ramirez <rramirez110911@gmail.com>
2025-06-06 19:30:56 -0700

    counts to find longest name in the list of champions

--- main.py
+++ main.py
@@ -5,9 +5,27 @@ def main():
     champion_list = fetch(
         "http://ddragon.leagueoflegends.com/cdn/12.6.1/data/en_US/champion.json"
     )
-    # for i in range(0, 4):
-    # print(champion_list[i])
-    print(champion_list[0])
+
+    counted = list(enumerate(champion_list, 1))
+
+    def format_list_of_champions(iterable):
+        str = ""
+        for item in iterable:
+            str += f"{item[0]}. {item[1]['name']} | "
+            if item[0] % 5 == 0:
+                str += "\n"
+
+        return str
+
+    print(format_list_of_champions(counted))
+    highest_count = 0
+    longest_name = ""
+    for champ in champion_list:
+        if len(champ["name"]) > highest_count:
+            highest_count = len(champ["name"])
+            longest_name = champ["name"]
+        continue
+    print(f"longest: {longest_name}\nlength: {highest_count}")
 
 
 if __name__ == "__main__":
