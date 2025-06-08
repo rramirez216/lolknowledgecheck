acb65122b52f3c82922670f5d9e220511e30baeb
Ruben Ramirez <rramirez110911@gmail.com>
2025-06-07 21:49:38 -0700

    updates the version of data dragon

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from helper_functions import append_string, pad_string
 
 def main():
     champion_list = fetch(
-        "http://ddragon.leagueoflegends.com/cdn/12.6.1/data/en_US/champion.json"
+        "http://ddragon.leagueoflegends.com/cdn/15.11.1/data/en_US/champion.json"
     )
 
     counted = list(enumerate(champion_list, 1))
@@ -15,18 +15,10 @@ def main():
             str += append_string(pad_string(item[1]["name"]), item[0])
             if item[0] % 5 == 0:
                 str += "\n"
-            # print(append_string(pad_string(item[1]["name"]), item[0]))
-            # print(pad_string(item[1]["name"]))
-
         return str
 
     print(format_list_of_champions(counted))
-    # TODO: find out out to make each entry to the str in format function the same length
-    # maybe while loop that checks if the string is == to the longest names length
-    # if not the same add spaces to the start and maybe end
 
-
-# "111._" "_|_"
 
 if __name__ == "__main__":
     main()
