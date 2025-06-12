de957921f641e8e44f32cab0919d77150333a3f5
Ruben Ramirez <rramirez110911@gmail.com>
2025-06-12 13:31:24 -0700

    creates a total_champions_prompt

--- constants.py
+++ constants.py
@@ -1,1 +1,11 @@
 roles_list = ["All", "support", "Marksman", "Mage", "Assassin", "Tank", "Fighter"]
+selection_prompt = (
+    "How would you like to select the champions for your test?\n"
+    "[R] - Start with a random set of champions\n"
+    "[M] - Choose specific champions from a list\n"
+    "Enter your choice (R/M):"
+)
+total_champions_prompt = (
+    "Select the total amount of champions you want to be tested on:\n"
+    "1. [5]    2. [10]    3. [15]"
+)
