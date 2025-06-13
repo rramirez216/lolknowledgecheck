ef9e74969b8c0253f9b81b2e9ce4d17d4322c88d
Ruben Ramirez <rramirez110911@gmail.com>
2025-06-12 21:47:14 -0700

    adds white space to the end of the first two prompts

--- constants.py
+++ constants.py
@@ -3,9 +3,12 @@ selection_prompt = (
     "How would you like to select the champions for your test?\n"
     "[R] - Start with a random set of champions\n"
     "[M] - Choose specific champions from a list\n"
-    "Enter your choice (R/M):"
+    "Enter your choice (R/M):  "
 )
 total_champions_prompt = (
-    "Select the total amount of champions you want to be tested on:\n"
-    "1. [5]    2. [10]    3. [15]"
+    "How many randomly selected champions do you want to be tested on:\n"
+    "1. [5]    2. [10]    3. [15]\n"
+    "Enter your choice (1/2/3):  "
 )
+select_filter_one = "Which option would you like to filter by, if none select 'All':"
+select_filter_two = "Enter your choice (1-7):  "
