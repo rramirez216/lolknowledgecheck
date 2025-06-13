55cf0ff6cdf7fea48bdc9101e577933a2bbb7e8a
Ruben Ramirez <rramirez110911@gmail.com>
2025-06-12 21:47:53 -0700

    defines create filter option function

--- inputs.py
+++ inputs.py
@@ -1,12 +1,29 @@
-from constants import selection_prompt, total_champions_prompt
+from constants import (
+    selection_prompt,
+    total_champions_prompt,
+    select_filter_one,
+    select_filter_two,
+    roles_list,
+)
 
 
 def choose_selection_method():
     selection_method = input(selection_prompt)
     print("method:", selection_method)
+    print("\n\n")
     return selection_method
 
 
 def choose_total_champions():
     total = input(total_champions_prompt)
     print(total)
+    print("\n\n")
+
+
+def choose_filter_option():
+    pairs = enumerate(roles_list, 1)
+    print(select_filter_one)
+    print("".join([f"{pair[0]}. {pair[1]}  " for pair in pairs]))
+    selected_option = input(select_filter_two)
+    print("filter by:", selected_option)
+    print("\n\n")
