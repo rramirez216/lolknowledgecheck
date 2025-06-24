f2c617892993884a49d3303e26f7860aa0045e13
Ruben Ramirez <rramirez110911@gmail.com>
2025-06-23 21:39:52 -0700

    defines a function to pass to the filter function

--- helper_functions.py
+++ helper_functions.py
@@ -24,9 +24,25 @@ def pad_string(string):
 def filter_list(iterable, tag):
     if tag == "All":
         return iterable
-    return list(filter(lambda champs: tag in champs["tags"], iterable))
+    print("we got this far")
+
+    def tag_filter(champ):
+        is_true = tag in champ["tags"]
+        print(champ["tags"])
+        return is_true
+
+    return list(filter(tag_filter, iterable))
 
 
-def randomly_pick_champions(champs, options):
-    pass
-    #
+def randomly_pick_champions(champ_list, options_tuple):
+    print(len(champ_list), options_tuple)
+    filtered_list = filter_list(champ_list, options_tuple[1])
+    print("filtered:", filtered_list)
+    new_champ_list = []
+    # for i in range(options_tuple[0]):
+    #     new_champ_list.append(filtered_list[randrange(len(filtered_list))])
+    # print(new_champ_list)
+    return new_champ_list
+
+
+# TODO: figure out why the Error "empty range for randrange()" is being printed
